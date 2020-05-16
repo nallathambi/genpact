131ad91345f30340db863304dfe3dc0b627882bc
Nallathambi <nallathambi.a98@gmail.com>
2020-05-16 17:55:14 +0530

    initial commit
    initial commit

---
+++ genpact-library-management/src/main/java/com/genpact/library/genpact/library/management/GenpactLibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.genpact.library.genpact.library.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenpactLibraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenpactLibraryManagementApplication.class, args);
+	}
+
+}
